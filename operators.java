3316e3fa32d88fb4779138a9b4c9614a6f489129
Janhvi Katakdhond <159972212+janhvi-24@users.noreply.github.com>
2026-02-10 20:50:35 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,12 @@
+public class operators{
+    public static void main(String args[]){
+        int n1 = 45, n2 = 89;
+
+        System.out.println("Arithmetic Operators:");
+        System.out.println("n1+n2 = "+(n1+n2));
+        System.out.println("n1-n2 = "+(n1-n2));
+        System.out.println("n1*n2 = "+(n1*n2));
+        System.out.println("n1/n2 = "+(n1/n2));
+        System.out.println("n1%n2 = "+(n1%n2));
+    }
+}
---
+++ variables.java
@@ -0,0 +1,13 @@
+public class variables{
+    public static void main(String args[]){
+        int num = 89;
+        String name = "MakeTechBerry LLP";
+        float floating_val = 5.42f;
+        boolean bool = true;
+
+        System.out.println("Int = "+ num);
+        System.out.println("String = "+ name);
+        System.out.println("Float ="+ floating_val);
+        System.out.println("Boolean = "+ bool);
+    }
+}
